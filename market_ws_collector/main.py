c16e8dee4909ba12d90ba81a831a5c1b8c4aac31
sunw43350 <sunw43350@gmail.com>
2025-07-21 11:14:04 +0000

    Auto commit: 2025-07-21 11:14:04

--- market_ws_collector/main.py
+++ market_ws_collector/main.py
@@ -11,3 +11,15 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+import asyncio
+from connectors import ascendex
+
+async def main():
+    # 可传入 symbols 或使用默认 config.py 中配置
+    connector = ascendex.Connector()
+    await connector.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
