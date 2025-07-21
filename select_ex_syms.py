77712ab1c7d4e4022798011853c14a282fe70af7
sunw43350 <sunw43350@gmail.com>
2025-07-21 03:55:16 +0000

    Auto commit: 2025-07-21 03:55:16

--- select_ex_syms.py
+++ select_ex_syms.py
@@ -7,3 +7,21 @@ symbol_exchanges = pickle.load(open('assets/symbol_exchanges.pkl', 'rb'))
 #     print(ex, len(syms))
 
 
+
+# 获取所有 symbol 和 exchange 的列表
+all_symbols = sorted(symbol_exchanges.keys())
+all_exchanges = sorted(exchange_symbols.keys())
+
+# 构建矩阵：行是 symbol，列是 exchange
+matrix = []
+for symbol in all_symbols:
+    row = []
+    for exchange in all_exchanges:
+        row.append(1 if symbol in exchange_symbols[exchange] else 0)
+    matrix.append(row)
+
+# 打印表头
+print("symbol," + ",".join(all_exchanges))
+# 打印每行
+for symbol, row in zip(all_symbols, matrix):
+    print(symbol + "," + ",".join(map(str, row)))
