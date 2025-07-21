ca82d0c9f55bca1b17b9bd06aa28ff3514e41df8
sunw43350 <sunw43350@gmail.com>
2025-07-21 16:07:21 +0000

    Auto commit: 2025-07-21 16:07:21

--- market_ws_collector/dispatcher/manager.py
+++ market_ws_collector/dispatcher/manager.py
@@ -1,4 +1,4 @@
-from connectors import ascendex, krakenfutures, bingx, bitfinex, bitget
+from connectors import ascendex, krakenfutures, bingx, bitfinex, bitget, bitmart
 from config import DEFAULT_SYMBOLS
 import asyncio
 
@@ -6,11 +6,12 @@ class ExchangeManager:
     def __init__(self, queue):
         self.queue = queue
         self.connectors = [
-            ascendex.Connector(exchange="ascendex", queue=queue),
-            krakenfutures.Connector(exchange="krakenfutures", queue=queue),
-            bingx.Connector(exchange="bingx", queue=queue),  # ✅ 添加 BingX
-            # bitfinex.Connector(exchange="bitfinex", queue=queue),  # ✅ 添加 Bitfinex  fail
-            bitget.Connector(exchange="bitget", queue=queue),  # ✅ 添加 Bitget
+            # ascendex.Connector(exchange="ascendex", queue=queue),
+            # krakenfutures.Connector(exchange="krakenfutures", queue=queue),
+            # bingx.Connector(exchange="bingx", queue=queue),  # ✅ 添加 BingX
+            # bitfinex.Connector(exchange="bitfinex", queue=queue),  #  添加 Bitfinex  fail
+            # bitget.Connector(exchange="bitget", queue=queue),  # ✅ 添加 Bitget
+            bitmart.Connector(exchange="bitmart", queue=queue)  # ✅ 添加 BitMart
 
             # 你可以继续添加 binance、bybit 等其他交易所
         ]
