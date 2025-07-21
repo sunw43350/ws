c4ef077be19a8b2caffc9b1528aed0236e952a9c
sunw43350 <sunw43350@gmail.com>
2025-07-21 03:31:42 +0000

    Auto commit: 2025-07-21 03:31:42

--- select_ex_syms.py
+++ select_ex_syms.py
@@ -4,5 +4,9 @@ import pickle
 exchange_symbols = pickle.load(open('assets/exchange_symbols.pkl', 'rb'))
 symbol_exchanges = pickle.load(open('assets/symbol_exchanges.pkl', 'rb'))
 
+
+for ex, syms in sorted(exchange_symbols.items(), key=lambda x: len(x[1])):
+    print(ex, len(syms))
+    
 for ex, syms in exchange_symbols.items():
     print(ex, len(syms))
