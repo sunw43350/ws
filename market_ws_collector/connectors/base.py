6c53a2c0bff8cce030d4afcade308144fe9ff213
sunw43350 <sunw43350@gmail.com>
2025-07-21 11:22:17 +0000

    Auto commit: 2025-07-21 11:22:17

--- market_ws_collector/connectors/base.py
+++ market_ws_collector/connectors/base.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 
-class BaseConnector(ABC):
+class BaseAsyncConnector(ABC):
     def __init__(self):
         self.exchange_name = self.__class__.__name__.replace("Connector", "")
 
     @abstractmethod
     def connect(self): pass
+
     @abstractmethod
     def subscribe(self, request): pass
+
     @abstractmethod
     def run_forever(self): pass
