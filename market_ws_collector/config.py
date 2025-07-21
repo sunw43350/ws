34defd2dcdbd2c3ddde89d1c2bc384617cc766c1
sunw43350 <sunw43350@gmail.com>
2025-07-21 11:04:48 +0000

    Auto commit: 2025-07-21 11:04:48

---
+++ market_ws_collector/config.py
@@ -0,0 +1,8 @@
+# market_ws_collector/config.py
+
+DEFAULT_SYMBOLS = {
+    "ascendex": ["BTC-USDT", "ETH-USDT", "SOL-USDT", "XRP-USDT", "LTC-USDT"],
+    "binance": ["BTC-USDT", "ETH-USDT"],
+    "bitmex": ["BTC-USDT", "ETH-USDT"]
+    # 其他交易所陆续添加
+}
