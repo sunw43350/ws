d3c69a8cec216429e8d5620df5e12bc548b062a0
sunw43350 <sunw43350@gmail.com>
2025-07-21 11:10:58 +0000

    Auto commit: 2025-07-21 11:10:58

--- market_ws_collector/config.py
+++ market_ws_collector/config.py
@@ -6,3 +6,11 @@ DEFAULT_SYMBOLS = {
     "bitmex": ["BTC-USDT", "ETH-USDT"]
     # 其他交易所陆续添加
 }
+
+
+WS_ENDPOINTS = {
+    "ascendex": "wss://ascendex.com/1/api/pro/v2/stream",
+    "binance": "wss://stream.binance.com:9443/ws",
+    "bitmex": "wss://www.bitmex.com/realtime"
+    # 后续交易所可继续添加
+}
