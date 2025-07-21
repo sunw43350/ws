db295b69aa4164934875c3faf644256d9b693a4c
sunw43350 <sunw43350@gmail.com>
2025-07-21 03:28:48 +0000

    Auto commit: 2025-07-21 03:28:48

--- select_ex_syms.py
+++ select_ex_syms.py
@@ -4,4 +4,4 @@ import pickle
 exchange_symbols = pickle.load(open('assets/exchange_symbols.pkl', 'rb'))
 symbol_exchanges = pickle.load(open('assets/symbol_exchanges.pkl', 'rb'))
 
-for ex in c
+for ex in exchange_symbols:
