4b24c9aad4d66febb4b972aa67a98dd6e6845d7f
sunw43350 <sunw43350@gmail.com>
2025-07-21 04:00:50 +0000

    Auto commit: 2025-07-21 04:00:50

--- select_ex_syms.py
+++ select_ex_syms.py
@@ -3,10 +3,6 @@ import pickle
 exchange_symbols = pickle.load(open('assets/exchange_symbols.pkl', 'rb'))
 symbol_exchanges = pickle.load(open('assets/symbol_exchanges.pkl', 'rb'))
 
-# for ex, syms in sorted(exchange_symbols.items(), key=lambda x: len(x[1]), reverse=True):
-#     print(ex, len(syms))
-
-
 # 过滤 symbol 数量小于 100 的 exchange
 filtered_exchanges = [ex for ex, syms in exchange_symbols.items() if len(syms) >= 100]
 filtered_exchanges = sorted(filtered_exchanges)
@@ -19,11 +15,15 @@ matrix = []
 for symbol in all_symbols:
     row = []
     for exchange in filtered_exchanges:
-        row.append(1 if symbol in exchange_symbols[exchange] else 0)
+        row.append(str(1 if symbol in exchange_symbols[exchange] else 0))
     matrix.append(row)
 
-# 打印表头
-print("symbol," + ",".join(filtered_exchanges))
+# 打印 Markdown 表头
+header = "| symbol | " + " | ".join(filtered_exchanges) + " |"
+separator = "|" + " --- |" * (len(filtered_exchanges) + 1)
+print(header)
+print(separator)
+
 # 打印每行
 for symbol, row in zip(all_symbols, matrix):
-    print(symbol + "," + ",".join(map(str, row)))
+    print("| " + symbol + " | " + " | ".join(row) + " |")
