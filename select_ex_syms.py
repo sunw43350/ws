f5a02f49aaecc26b1d3edf78bbc87cbdd14bbd0f
sunw43350 <sunw43350@gmail.com>
2025-07-21 03:37:10 +0000

    Auto commit: 2025-07-21 03:37:10

--- select_ex_syms.py
+++ select_ex_syms.py
@@ -5,3 +5,15 @@ symbol_exchanges = pickle.load(open('assets/symbol_exchanges.pkl', 'rb'))
 
 for ex, syms in sorted(exchange_symbols.items(), key=lambda x: len(x[1]), reverse=True):
     print(ex, len(syms))
+
+
+# 获取所有 symbol 列表
+all_symbols = sorted({symbol for syms in exchange_symbols.values() for symbol in syms})
+
+# 打印表头
+print("exchange," + ",".join(all_symbols))
+
+# 打印每个交易所对应的 symbol 行
+for ex in sorted(exchange_symbols.keys()):
+    row = [str(int(symbol in exchange_symbols[ex])) for symbol in all_symbols]
+    print(f"{ex}," + ",".join(row))
