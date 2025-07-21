add09d8b63a0dafa07ce24ee28a80bb6f431513a
sunw43350 <sunw43350@gmail.com>
2025-07-21 03:38:22 +0000

    Auto commit: 2025-07-21 03:38:22

--- select_ex_syms.py
+++ select_ex_syms.py
@@ -7,13 +7,18 @@ symbol_exchanges = pickle.load(open('assets/symbol_exchanges.pkl', 'rb'))
 #     print(ex, len(syms))
 
 
-# 获取所有 symbol 列表
+# 获取所有交易所和所有symbol
+exchanges = list(exchange_symbols.keys())
 all_symbols = sorted({symbol for syms in exchange_symbols.values() for symbol in syms})
 
-# 打印表头
-print("exchange," + ",".join(all_symbols))
+# 构建矩阵
+matrix = []
+for ex in exchanges:
+    row = [1 if symbol in exchange_symbols[ex] else 0 for symbol in all_symbols]
+    matrix.append(row)
 
-# 打印每个交易所对应的 symbol 行
-for ex in sorted(exchange_symbols.keys()):
-    row = [str(int(symbol in exchange_symbols[ex])) for symbol in all_symbols]
-    print(f"{ex}," + ",".join(row))
+# 打印表头
+print("exchange,", ",".join(all_symbols))
+# 打印每一行
+for ex, row in zip(exchanges, matrix):
+    print(f"{ex}," + ",".join(map(str, row)))
