b462c367051c7dd8c58376ee3d24f26fddb7d33b
sunw43350 <sunw43350@gmail.com>
2025-07-22 14:03:05 +0000

    Auto commit: 2025-07-22 14:03:05

--- market_ws_collector/main.py
+++ market_ws_collector/main.py
@@ -10,7 +10,7 @@ from dispatcher.manager import ExchangeManager
 
 # Configurations
 DATA_RETENTION_MINUTES = 20
-PLOT_INTERVAL_SECONDS = 60*10
+PLOT_INTERVAL_SECONDS = 60*5
 
 # Runtime storage
 active_symbols = set()
