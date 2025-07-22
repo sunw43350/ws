a88ddb30663f83b312e6db9b398010d8a310cacf
sunw43350 <sunw43350@gmail.com>
2025-07-22 02:02:47 +0000

    Auto commit: 2025-07-22 02:02:47

--- market_ws_collector/models/base.py
+++ market_ws_collector/models/base.py
@@ -4,15 +4,6 @@ class SubscriptionRequest:
         self.channel = channel
         self.depth_level = depth_level
 
-class MarketSnapshot:
-    def __init__(self, exchange, symbol, best_bid, best_ask, timestamp):
-        self.exchange = exchange
-        self.symbol = symbol
-        self.best_bid = best_bid
-        self.best_ask = best_ask
-        self.timestamp = timestamp
-
-from datetime import datetime
 
 class MarketSnapshot:
     def __init__(
