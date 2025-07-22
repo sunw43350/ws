219d7159052769fbc2546648fa557e531220bbdc
sunw43350 <sunw43350@gmail.com>
2025-07-22 05:44:50 +0000

    Auto commit: 2025-07-22 05:44:50

--- market_ws_collector/dispatcher/manager.py
+++ market_ws_collector/dispatcher/manager.py
@@ -17,7 +17,7 @@ class ExchangeManager:
         self.queue = queue
         self.connectors = [
             # ascendex.Connector(exchange="ascendex", queue=queue),
-            # krakenfutures.Connector(exchange="krakenfutures", queue=queue),
+            krakenfutures.Connector(exchange="krakenfutures", queue=queue),
             # bingx.Connector(exchange="bingx", queue=queue),  # ✅ 添加 BingX
             # bitget.Connector(exchange="bitget", queue=queue),  # ✅ 添加 Bitget
             # bitmart.Connector(exchange="bitmart", queue=queue),  # ✅ 添加 BitMart
@@ -42,7 +42,8 @@ class ExchangeManager:
             # 你可以继续添加 binance、bybit 等其他交易所
         ]
 
-    def 
+    def load_connectors(self):
+        pass
 
     async def run_all(self):
         tasks = [asyncio.create_task(conn.run()) for conn in self.connectors]
