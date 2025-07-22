6d4b3e247489d2eb8e70e9c4ee96a58650c0390a
sunw43350 <sunw43350@gmail.com>
2025-07-22 11:02:01 +0000

    Auto commit: 2025-07-22 11:02:01

--- selector/select_syms.py
+++ selector/select_syms.py
@@ -87,7 +87,7 @@ to_delete = []
 for ex in top_100_exchange_symbols:
     num = len(top_100_exchange_symbols[ex])
     print(f"交易所 {ex} 支持的前 100 个 symbol 数量: {num}")
-    if num < 10:
+    if num < 5:
         to_delete.append(ex)
         print(f"⚠️ 警告: 交易所 {ex} 支持的前 100 个 symbol 数量少于 10 个！")
 
