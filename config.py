b1fce031c10c7584cebac6d6701ccd2b313f2e8e
sunw43350 <sunw43350@gmail.com>
2025-07-22 04:38:06 +0000

    Auto commit: 2025-07-22 04:38:06

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-
-select_
+SELECT_EXCHANGES = [
+    "ascendex", "krakenfutures", "bingx", "bitfinex", "bitget", "bitmart", "bitmex",
+    "bitrue", "blofin", "bybit", "coinbase", "cryptocom", "digifinex", "gateio",
+    "huobi", "lbank", "mexc", "okx", "oxfun", "phemex"
+]
