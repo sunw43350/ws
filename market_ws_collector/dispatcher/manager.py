1297a292f05daa640b3924b35663a4b837705766
sunw43350 <sunw43350@gmail.com>
2025-07-22 03:10:53 +0000

    Auto commit: 2025-07-22 03:10:53

--- market_ws_collector/dispatcher/manager.py
+++ market_ws_collector/dispatcher/manager.py
@@ -14,26 +14,28 @@ class ExchangeManager:
     def __init__(self, queue):
         self.queue = queue
         self.connectors = [
-            ascendex.Connector(exchange="ascendex", queue=queue),
-            krakenfutures.Connector(exchange="krakenfutures", queue=queue),
-            bingx.Connector(exchange="bingx", queue=queue),  # ✅ 添加 BingX
-            # bitfinex.Connector(exchange="bitfinex", queue=queue),  #  添加 Bitfinex  fail slow
-            bitget.Connector(exchange="bitget", queue=queue),  # ✅ 添加 Bitget
-            bitmart.Connector(exchange="bitmart", queue=queue),  # ✅ 添加 BitMart
-            bitmex.Connector(exchange="bitmex", queue=queue),  # ✅ 添加 BitMEX
-            bitrue.Connector(exchange="bitrue", queue=queue),  # ✅ 添加 Bitrue
-            blofin.Connector(exchange="blofin", queue=queue),  # ✅ 添加 BloFin
-            bybit.Connector(exchange="bybit", queue=queue),  # ✅ 添加 Bybit
-            coinbase.Connector(exchange="coinbase", queue=queue),  # ✅ 添加 Coinbase
-            cryptocom.Connector(exchange="cryptocom", queue=queue),  # ✅ 添加 Crypto.
-            digifinex.Connector(exchange="digifinex", queue=queue),  # ✅ 添加 Digifinex
-            gateio.Connector(exchange="gateio", queue=queue),  # ✅ 添加 Gate.io
-            huobi.Connector(exchange="huobi", queue=queue),  # ✅ 添加 Huobi
-            lbank.Connector(exchange="lbank", queue=queue),  # ✅ 添加 LBank  LBank 异常: sent 1011 (internal error) keepalive ping timeout; no close frame received
-            mexc.Connector(exchange="mexc", queue=queue),  # ✅ 添加 MEXC slow update/1 second
-            okx.Connector(exchange="okx", queue=queue),  # ✅ 添加 OKX
-            oxfun.Connector(exchange="oxfun", queue=queue),  # ✅ 添加 OX.FUN
-            phemex.Connector(exchange="phemex", queue=queue),  # ✅ 添加 Phemex
+            # ascendex.Connector(exchange="ascendex", queue=queue),
+            # krakenfutures.Connector(exchange="krakenfutures", queue=queue),
+            # bingx.Connector(exchange="bingx", queue=queue),  # ✅ 添加 BingX
+            # bitget.Connector(exchange="bitget", queue=queue),  # ✅ 添加 Bitget
+            # bitmart.Connector(exchange="bitmart", queue=queue),  # ✅ 添加 BitMart
+            # bitmex.Connector(exchange="bitmex", queue=queue),  # ✅ 添加 BitMEX
+            # bitrue.Connector(exchange="bitrue", queue=queue),  # ✅ 添加 Bitrue
+            # blofin.Connector(exchange="blofin", queue=queue),  # ✅ 添加 BloFin
+            # bybit.Connector(exchange="bybit", queue=queue),  # ✅ 添加 Bybit
+            # coinbase.Connector(exchange="coinbase", queue=queue),  # ✅ 添加 Coinbase
+            # cryptocom.Connector(exchange="cryptocom", queue=queue),  # ✅ 添加 Crypto.
+            # digifinex.Connector(exchange="digifinex", queue=queue),  # ✅ 添加 Digifinex
+            # gateio.Connector(exchange="gateio", queue=queue),  # ✅ 添加 Gate.io
+            # huobi.Connector(exchange="huobi", queue=queue),  # ✅ 添加 Huobi
+            # lbank.Connector(exchange="lbank", queue=queue),  # ✅ 添加 LBank  LBank 异常: sent 1011 (internal error) keepalive ping timeout; no close frame received
+            # mexc.Connector(exchange="mexc", queue=queue),  # ✅ 添加 MEXC slow update/1 second
+            # okx.Connector(exchange="okx", queue=queue),  # ✅ 添加 OKX
+            # oxfun.Connector(exchange="oxfun", queue=queue),  # ✅ 添加 OX.FUN
+            # phemex.Connector(exchange="phemex", queue=queue),  # ✅ 添加 Phemex
+
+            bitfinex.Connector(exchange="bitfinex", queue=queue),  #  添加 Bitfinex  fail slow
+
 
 
             # 你可以继续添加 binance、bybit 等其他交易所
