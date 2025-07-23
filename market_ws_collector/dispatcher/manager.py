ef9c3aa9d2615025970a9357deadfb33f9cc7e8d
sunw43350 <sunw43350@gmail.com>
2025-07-23 01:59:59 +0000

    up 2025-07-23 01:59:59

--- market_ws_collector/dispatcher/manager.py
+++ market_ws_collector/dispatcher/manager.py
@@ -38,7 +38,7 @@ class ExchangeManager:
             # huobi.Connector(exchange="huobi", queue=queue),  # ✅ 添加 Huobi
             # lbank.Connector(exchange="lbank", queue=queue),  # ✅ 添加 LBank  LBank 异常: sent 1011 (internal error) keepalive ping timeout; no close frame received
             # mexc.Connector(exchange="mexc", queue=queue),  # ✅ 添加 MEXC slow update/1 second
-            # okx.Connector(exchange="okx", queue=queue),  # ✅ 添加 OKX
+            okx.Connector(exchange="okx", queue=queue),  # ✅ 添加 OKX
             # oxfun.Connector(exchange="oxfun", queue=queue),  # ✅ 添加 OX.FUN
             # phemex.Connector(exchange="phemex", queue=queue),  # ✅ 添加 Phemex
 
