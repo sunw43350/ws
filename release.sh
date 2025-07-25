23bbced0097ed38f23618b24e6bf6d3e68b8c48c
sunw43350 <sunw43350@gmail.com>
2025-07-25 07:57:41 +0000

    up 2025-07-25 07:57:41

--- release.sh
+++ release.sh
@@ -1,2 +1,14 @@
-gh release create v1.2 --title "v1.2-alpha" --notes "修复bug，增加画图功能"
+#!/bin/bash
+TAG="v1.2.2"
+TITLE=$TAG"-alpha"
+NOTES="整理框架，增加图像保存功能，和csv保存功能"
+gh release create "$TAG" --title "$TITLE" --notes "$NOTES"
+REPO_URL=$(gh repo view --json nameWithOwner -q '.nameWithOwner')
+echo "Source ZIP:"
+echo "https://github.com/$REPO_URL/archive/refs/tags/$TAG.zip"
 
+# gh release create v1.2.1 --title "v1.2.1-alpha" --notes "整理框架，增加图像保存功能，和csv保存功能"
+
+
+# https://github.com/sunw43350/ws/releases/tag/v1.2.1
+# https://github.com/sunw43350/ws/archive/refs/tags/v1.2.1.zip
